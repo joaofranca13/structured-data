0c86f7abbe549835844970743b648a003d4d7ec2
João França <joao.francapf@gmail.com>
2020-06-28 10:39:17 -0300

    Solved exercises

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -53,7 +53,7 @@
   (count (get book :authors)))
 
 (defn multiple-authors? [book]
-  (if (> (author-count) 1) true false))
+  (if (> (author-count book) 1) true false))
 
 (defn add-author [book new-author]
   (let [author (get book :authors) 
@@ -85,45 +85,61 @@
   (if (contains? a-set elem) (disj a-set elem) (conj a-set elem)))
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (if (> (count a-seq) (count (set a-seq))) true false))
 
 (defn old-book->new-book [book]
-  :-)
+  (assoc book :authors (set (:authors book))))
 
 (defn has-author? [book author]
-  :-)
+  (if (contains? (get book :authors) author) true false))
 
 (defn authors [books]
-  :-)
+  (apply clojure.set/union (map :authors books)))
 
 (defn all-author-names [books]
-  :-)
+  (set (map :name (authors books))))
 
 (defn author->string [author]
-  :-)
+  (let [author-name
+        (:name author)
+        birth-date
+        (:birth-year author)
+        death-date
+        (:death-year author)]
+    (cond
+      (:death-year author) (str author-name " (" birth-date " - " death-date ")")
+      (:birth-year author) (str author-name " (" birth-date " - )")
+      :else author-name)))
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  :-)
+  (let [titulo
+        (:title book)
+        autor
+        (authors->string (:authors book))]
+    (apply str (interpose ", written by " [titulo autor]))))
 
 (defn books->string [books]
-  :-)
+  (cond
+    (= (count books) 1) (apply str (count books)" book. "(apply str (map book->string books)) ".")
+    (> (count books) 1) (apply str (count books)" books. "(apply str (interpose " | "(map book->string books)))".")
+    :else "No books."))
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [book] (has-author? book author)) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (first (filter (fn [author] (= name (:name author))) authors)))
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  :-)
+  (not (empty? (filter alive? (:authors book)))))
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter (fn [book] (has-a-living-author? book)) books))
 
 ; %________%
